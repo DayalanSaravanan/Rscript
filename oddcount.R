cae066eabb24472a89d7f0411f162052a1cbebda
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-10-27 20:15:55 +0530

    script to count the number of odd integers

---
+++ oddcount.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+# File: oddcount.R
+# Name: D.Saravanan
+# Date: 22/10/2020
+# Script to count the number of odd integers
+
+oddcount <- function(x) {
+	n <- 0
+
+	for (m in x) {
+		if (m %% 2 == 1) n <- n + 1
+	}
+
+	return(n)
+}
+
+oddcount(c(1, 2, 3, 7, 9, 15))
