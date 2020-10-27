8628ca00a8db27101974c5084d2d04fca2e3dc95
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-10-27 20:14:40 +0530

    script to count the number of even integers

---
+++ evencount.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+# File: evencount.R
+# Name: D.Saravanan
+# Date: 22/10/2020
+# Script to count the number of even integers
+
+evencount <- function(x) {
+	n <- 0
+
+	for (m in x) {
+		if (m %% 2 == 0) n <- n + 1
+	}
+
+	return(n)
+}
+
+evencount(c(1, 2, 3, 7, 9, 15))
