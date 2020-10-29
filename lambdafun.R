6cc6bcc7d82b06e48f7edd0ec49433a91732f453
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-10-29 21:43:19 +0530

    script using lambda function

---
+++ lambdafun.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+# File: lambdafun.R
+# Name: D.Saravanan
+# Date: 29/10/2020
+# Script using lambda function
+
+(function(x) x**2) (41)
+
+square <- (function(x) x**2)
+square(41)
+
+(function(x, y) x**y) (7, 2)
+
+N = (function(x, y) x**y)
+N(7, 2)
