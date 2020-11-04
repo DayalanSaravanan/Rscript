692f913c9348edaca207888d7da5dc5002aeda1f
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-11-04 19:29:20 +0530

    script to compute binomial distribution

---
+++ binomial.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+# File: binomial.R
+# Name: D.Saravanan
+# Date: 31/10/2020
+# Script for binomial distribution
+
+# factorial
+fact <- function(n) {
+	fact <- 1
+	for (value in 1:n) {
+		fact <- fact * value
+	}
+	return(fact)
+}
+
+binomial <- function(k, n, p) {
+	result = (fact(n)/(fact(k) * fact(n-k))) * p**k * (1 - p)**(n-k)
+	return(result)
+}
+
+
